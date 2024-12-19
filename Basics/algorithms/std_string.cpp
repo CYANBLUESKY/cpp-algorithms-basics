9606dc7c76fedb06ce37ce1d9aa57784954e4be7
CYANBLUESKY <username@users.noreply.github.com>
2024-12-19 09:00:16 +0900

    Study about string

---
+++ Basics/algorithms/std_string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string string_value("Hello World!");
+	cout << string_value << endl;
+
+	string_value = "My name is CYANBLUESKY!";
+	cout << string_value << endl;
+
+	return 0;
+}
+
+// Study about String
+// 문자열 관련 예제
